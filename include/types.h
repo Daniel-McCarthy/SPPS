93668df86c51939193e1d4645ed854ec22ac5961
Dan McCarthy <dan.mccarthy.software@gmail.com>
2025-05-11 23:51:35 -0500

    Include types.h to include type defs

---
+++ include/types.h
@@ -0,0 +1,19 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef signed short s16;
+typedef unsigned short u16;
+typedef signed char s8;
+typedef unsigned char u8;
+typedef signed int s32;
+typedef unsigned int u32;
+typedef signed long s64;
+typedef unsigned long u64;
+typedef float f32;
+typedef double f64;
+typedef unsigned int u_int;
+typedef __int128 int128;
+typedef __int128 s128;
+typedef unsigned __int128 u_int128;
+
+#endif
